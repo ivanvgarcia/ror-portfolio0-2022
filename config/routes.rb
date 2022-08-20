6a9d3f338471c5a3487f744614dd94920ca3583e
Ivan Garcia <ivanvlora@gmail.com>
2022-08-20 13:00:51 +0900

    [feature] error page, new design

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
   devise_for :users
   get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "projects#index"
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 
   resources :projects
 end
