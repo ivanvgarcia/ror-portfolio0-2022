cc23a19715c5d281de6d4e838b0c6c6c555a61da
Ivan Garcia <ivanvlora@gmail.com>
2022-08-23 19:45:43 +0900

    [feature] mobile menu button blog

--- app/frontend/entrypoints/controllers/blog/header_controller.js
+++ app/frontend/entrypoints/controllers/blog/header_controller.js
@@ -1,7 +1,27 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["menuOverlay"];
+
   connect() {
     console.log("Hello, Stimulus!", this.element);
+  }
+
+  openMenu() {
+    this.menuOverlayTarget.classList.add("opacity-100", "visible", "ease-in");
+    this.menuOverlayTarget.classList.remove(
+      "opacity-0",
+      "invisible",
+      "ease-out"
+    );
+  }
+
+  closeMenu() {
+    this.menuOverlayTarget.classList.add("opacity-0", "invisible", "ease-out");
+    this.menuOverlayTarget.classList.remove(
+      "opacity-100",
+      "visible",
+      "ease-in"
+    );
   }
 }
